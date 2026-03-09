04c806fc696cd1ea711967c4b7a47365793ca35d
wcDhruvi <wc.dhruvi@gmail.com>
2026-03-09 12:36:45 +0530

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <h1 className="text-xl font-bold text-blue-600">
+          Product Finder
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
